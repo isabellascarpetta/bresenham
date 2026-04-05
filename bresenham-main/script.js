955e24d9d640519df06848ed7b96f34af724a5cf
Isabella Scarpetta <est.isabella.scarp@unimilitar.edu.co>
2026-04-04 22:45:40 -0500

    Se implementa la funcion bresenhamAdaptado con registro de pasos para tabla

--- bresenham-main/script.js
+++ bresenham-main/script.js
@@ -36,3 +36,35 @@ function prepararCanvas() {
 }
 
 prepararCanvas();
+
+function bresenhamAdaptado(x0, y0, x1, y1, ctx) {
+    let puntos = []; // Aquí guardaremos los pasos
+    
+    let dx = Math.abs(x1 - x0);
+    let dy = Math.abs(y1 - y0);
+    let sx = (x0 < x1) ? 1 : -1;
+    let sy = (y0 < y1) ? 1 : -1;
+    let err = dx - dy;
+
+    while (true) {
+        puntos.push({ x: x0, y: y0, error: err });
+
+        // Dibujar el punto 
+        ctx.fillRect(x0, y0, 2, 2);
+
+        if (x0 === x1 && y0 === y1) break;
+
+        let e2 = 2 * err;
+
+        if (e2 > -dy) {
+            err -= dy;
+            x0 += sx;
+        }
+
+        if (e2 < dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
+    return puntos;
+}
